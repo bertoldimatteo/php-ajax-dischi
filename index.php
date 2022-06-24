3d1ef17150bbdfbbd181f7351c1ec86780838034
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-24 16:11:22 +0200

    aggiunto echo + stile css

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+
+include './server/database.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>php-dischi</title>
+</head>
+<body>
+    <div class="container">
+        <?php foreach($database as $music) {?>
+            <div class="card">
+                <img class="poster-album" src="<?php echo $music['poster']; ?>" alt="">
+                <h2><?php echo $music['title']; ?></h2>
+                <p><?php echo $music['author']; ?></p>
+                <span><?php echo $music['year']?></span>
+            </div>      
+        <?php } ?>
+    </div>
+</body>
+</html>
